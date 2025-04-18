d770db9327fb5f47f9a8efe7d0db3710a47fba6b
Tyra <tyra.mwai@student.moringaschool.com>
2025-04-18 04:19:21 +0300

    Completed my assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
-import React from "react";
-import Header from "./Header";
-import PlantPage from "./PlantPage";
+import React, { useEffect, useState } from "react";
+import PlantList from "./PlantList";
 
 function App() {
+  const [plants, setPlants] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((res) => res.json())
+      .then((data) => setPlants(data))
+      .catch((err) => console.error("Error fetching plants:", err));
+  }, []);
+
   return (
-    <div className="app">
-      <Header />
-      <PlantPage />
+    <div className="App">
+      <h1>Plantsy 🌿</h1>
+      <PlantList plants={plants} />
     </div>
   );
 }
